f034faec794fc33d0f6e51c29d883f124fcf3cbd
jwojcik <j.wojcik1994@gmail.com>
2022-03-24 12:07:52 +0100

    Fix1

--- start.js
+++ start.js
@@ -1,21 +1,18 @@
-<<<<<<< HEAD
 console.log("Hello World");
 console.log("!");
 
 function goodMorning() {
-    for (let i = 0; i < 10; i++) {
-        console.log('dzień dobry');
-    }
+  for (let i = 0; i < 10; i++) {
+    console.log("dzień dobry");
+  }
 
-    console.log('jade na orlowo cos przekasic');
-    console.log('to nie 2005');
+  console.log("jade na orlowo cos przekasic");
+  console.log("to nie 2005");
 }
 
 goodMorning();
-=======
 const HelloWorld = () => {
   console.log("Hello World!");
 };
 
 console.log(1 + 2);
->>>>>>> e7c033e3499b869d7c95c7343e88cc3d9d959225
