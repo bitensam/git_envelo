b935e8cd6f9a2156765d71b8920d7faec659a1b9
SuperBrzezinski <michal.a.brzezinski@gmail.com>
2022-03-24 11:43:10 +0100

    Created function goodMorning()
    function lets user to welcome his listeners

--- start.js
+++ start.js
@@ -1,2 +1,10 @@
 console.log("Hello World");
 console.log("!");
+
+function goodMorning() {
+    for (let i = 0; i < 10; i++) {
+        console.log('dzień dobry');
+    }
+
+    log.console('jade na orlowo cos przekasic')
+}
